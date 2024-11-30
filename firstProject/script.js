5632dd8b04bb21a83d598ed658336ad1e69648de
ArbindDas <dasarbind269@gmail.com>
2024-11-30 17:40:15 +0545

    done

---
+++ firstProject/script.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+const bgContainer = document.querySelector('.bgContainer');
+const colorItems = document.querySelectorAll('.colorItems');
+
+const firstColor = document.getElementById('red');
+const secondColor = document.getElementById('black');
+const thirdColor = document.getElementById('blue');
+const fourthColor = document.getElementById('green');
+const fifthColor = document.getElementById('brown');
