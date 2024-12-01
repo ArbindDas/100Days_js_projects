eb809ec15894b039a0443d23c80b103b54345248
ArbindDas <dasarbind269@gmail.com>
2024-12-01 17:07:34 +0545

    done

---
+++ thirdProject/script.js
@@ -0,0 +1,28 @@
+(function () {
+  const buttons = document.querySelectorAll(".btn-counter");
+  let count = 0;
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      if (button.classList.contains("increase-btn")) {
+        if (count >= 19) {
+          alert("you can't add more then  values : " + count);
+          return;
+        } else {
+          count++;
+        }
+      }
+      if (button.classList.contains("decrease-btn")) {
+        if (count <= 0) {
+          alert("you can't remove values : " + count);
+          return;
+        } else {
+          count--;
+        }
+      }
+
+      const counter = document.querySelector(".counter");
+      counter.textContent = count;
+    });
+  });
+})();
