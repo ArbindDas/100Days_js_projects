efb8e81a51775f025a6a3a4d8d4f1435ad26c689
ArbindDas <dasarbind269@gmail.com>
2024-12-01 17:26:05 +0545

    done with third projects

--- thirdProject/script.js
+++ thirdProject/script.js
@@ -1,28 +1,61 @@
-(function () {
-  const buttons = document.querySelectorAll(".btn-counter");
+// (function () {
+//   const buttons = document.querySelectorAll(".btn-counter");
+//   let count = 0;
+
+//   buttons.forEach(function (button) {
+//     button.addEventListener("click", function () {
+//       if (button.classList.contains("increase-btn")) {
+//         if (count >= 19) {
+//           alert("you can't add more then  values : " + count);
+//           return;
+//         } else {
+//           count++;
+//         }
+//       }
+//       if (button.classList.contains("decrease-btn")) {
+//         if (count <= 0) {
+//           alert("you can't remove values : " + count);
+//           return;
+//         } else {
+//           count--;
+//         }
+//       }
+
+//       const counter = document.querySelector(".counter");
+//       counter.textContent = count;
+//     });
+//   });
+// })();
+
+
+
+function printCounter(){
+  const buttons = document.querySelectorAll('.btn-counter');
   let count = 0;
+  buttons.forEach(function (button){
+      button.addEventListener("click" , function(){
+          if (button.classList.contains("increase-btn")) {
+            if (count>=19) {
+              alert("you can't add more then : "+count);
+              return;
+            }else{
+              count++;
+            }
+          }
 
-  buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      if (button.classList.contains("increase-btn")) {
-        if (count >= 19) {
-          alert("you can't add more then  values : " + count);
-          return;
-        } else {
-          count++;
-        }
-      }
-      if (button.classList.contains("decrease-btn")) {
-        if (count <= 0) {
-          alert("you can't remove values : " + count);
-          return;
-        } else {
-          count--;
-        }
-      }
+          if (button.classList.contains("decrease-btn")) {
+            if (count<=0) {
+              alert("you can't remove more then : "+count);
+              return;
+            }else{
+              count--;
+            }
+          }
 
-      const counter = document.querySelector(".counter");
-      counter.textContent = count;
-    });
+          const counterResult = document.querySelector(".counter");
+          counterResult.textContent = count;
+      });
   });
-})();
+};
+
+printCounter();
